0a42d9411d0c49102daa407e6bf37d0455ceca96
Ghulam Mohiyuddin <ghulam.178204@knit.ac.in>
2023-01-30 21:50:48 +0530

    removing Route and deploying to github

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import { useState } from 'react';
 import Alert from './components/Alert';
-import About from './components/About';
+// import About from './components/About';
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
-} from 'react-router-dom';
+// import {
+// 	BrowserRouter as Router,
+// 	Routes,
+// 	Route,
+// 	Link
+// } from 'react-router-dom';
 
 
 function App() {
@@ -49,26 +49,26 @@ setTimeout(() => {alertfun(null)
     }
   }
   return (
-    <Router>
+    // <Router>
     <>
-    <Navbar mode={mode} toggle={toggleMode} showAlert= {showAlert}/>
+    {/* <Navbar mode={mode} toggle={toggleMode} showAlert= {showAlert}/>
     <Alert alert={alert}/>
     <div className='container'>
     <Routes>
                  <Route exact path='/' element={< Form mode={mode} showAlert={showAlert}/>}></Route>
                  <Route exact path='/about' element={< About />}></Route>
     </Routes>
-    </div>
+    </div> */}
 
-    {/* <Navbar mode={mode} toggle={toggleMode} showAlert= {showAlert}/>
+    <Navbar mode={mode} toggle={toggleMode} showAlert= {showAlert}/>
     <Alert alert={alert}/>
     <div className='container'>
     <Form mode={mode} showAlert={showAlert}/>
     </div>
-    <About/> */}
+    {/* <About/> */}
 
+    {/* </Router> */}
     </>
-    </Router>
     
   );
 }
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    props.alert&&
+    props.alert&& 
     <div>
         <div className={`alert alert-${props.alert.type}`} role="alert">
     <strong>{props.alert.msg}</strong>
